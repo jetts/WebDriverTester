b0379283e744715e236a7f346e4d8c5feb753b4f
Clayton Martin <instylevii@outlook.com>
2016-01-20 23:04:59 -0800

    Added another set of commands

--- scripts/main.js
+++ scripts/main.js
@@ -9,9 +9,9 @@
 			commandName: 'acceptAlert',
 			commandTitle: 'Accept Alert',
 			method: 'POST',
-			path: '/session/SESSION_ID/accept_alert', // /session/{session id}/alert/accept
+			path: '/session/SESSION_ID/accept_alert',
 			requestBody: '',
-			description: 'W3C: The Accept Alert command accepts a simple dialogue if present.'
+			description: 'JSON Wire Protocol: Accepts the currently displayed alert dialog. Usually, this is equivalent to clicking on the \'OK\' button in the dialog.'
 		},
 		{
 			commandName: 'addCookie',
@@ -89,25 +89,25 @@
 			commandName: 'dismissAlert',
 			commandTitle: 'Dismiss Alert',
 			method: 'POST',
-			path: '/session/SESSION_ID/dismiss_alert', // /session/{session id}/alert/dismiss
+			path: '/session/SESSION_ID/dismiss_alert',
 			requestBody: '',
-			description: 'W3C: The Dismiss Alert command dismisses a simple dialogue if present. A request to dismiss an alert user prompt, which may not necessarily have a dismiss button, has the same effect as accepting it.'
+			description: 'JSON Wire Protocol: Dismisses the currently displayed alert dialog. For confirm() and prompt() dialogs, this is equivalent to clicking the \'Cancel\' button. For alert() dialogs, this is equivalent to clicking the \'OK\' button.'
 		},
 		{
 			commandName: 'executeScript',
 			commandTitle: 'Execute Script',
 			method: 'POST',
-			path: '/session/SESSION_ID/execute', // /session/{session id}/execute/sync
+			path: '/session/SESSION_ID/execute',
 			requestBody: '{\"script\": \"\",\"args\": []}',
-			description: 'W3C: The Execute Script command executes a JavaScript function in the context of the current browsing context and returns the return value of the function.'
+			description: 'JSON Wire Protocol: Inject a snippet of JavaScript into the page for execution in the context of the currently selected frame. The executed script is assumed to be synchronous and the result of evaluating the script is returned to the client.'
 		},
 		{
 			commandName: 'executeAsyncScript',
 			commandTitle: 'Execute Async Script',
 			method: 'POST',
-			path: '/session/SESSION_ID/execute_async', // /session/{session id}/execute/async
+			path: '/session/SESSION_ID/execute_async',
 			requestBody: '{\"script\": \"arguments[1]([ document.getElementsByTagName(\'div\'), 1, \'fancy string\', 1.2, {objProp: 3}]);\",\"args\": [{\"first\":\"1st\", \"second\":\"2nd\", \"third\":\"3rd\"}]}',
-			description: 'W3C: The Execute Async Script command causes JavaScript to execute as an anonymous function. Unlike the Execute Script command, the result of the function is ignored. Instead an additional argument is provided as the final argument to the function. This is a function that, when called, returns its first argument as the response.'
+			description: 'JSON Wire Protocol: Inject a snippet of JavaScript into the page for execution in the context of the currently selected frame. The executed script is assumed to be asynchronous and must signal that is done by invoking the provided callback, which is always provided as the final argument to the function. The value to this callback will be returned to the client.'
 		},
 		{
 			commandName: 'findElement',
@@ -161,9 +161,9 @@
 			commandName: 'getAlertText',
 			commandTitle: 'Get Alert Text',
 			method: 'GET',
-			path: '/session/SESSION_ID/alert_text', // /session/{session id}/alert/text
+			path: '/session/SESSION_ID/alert_text',
 			requestBody: '',
-			description: 'W3C: The Get Alert Text command returns the message of the current user prompt. If there is no user prompt, it returns with null.'
+			description: 'JSON Wire Protocol: Gets the text of the currently displayed JavaScript alert(), confirm(), or prompt() dialog.'
 		},
 		{
 			commandName: 'getCapabilities',
@@ -182,7 +182,7 @@
 			description: 'W3C: The Get Cookie command returns all cookies associated with the address of the current browsing context’s active document.'
 		},
 		{
-			commandName: 'Get All Cookies',
+			commandName: 'getAllCookies',
 			commandTitle: 'Get All Cookies',
 			method: 'GET',
 			path: '/session/SESSION_ID/cookie',
@@ -197,29 +197,61 @@
 			requestBody: '',
 			description: 'W3C: The Get Element CSS Value command retrieves the computed value of the given CSS property of the given web element.'
 		},
+		{
+			commandName: 'getCurrentUrl',
+			commandTitle: 'Get Current URL',
+			method: 'GET',
+			path: '/session/SESSION_ID/url',
+			requestBody: '',
+			description: 'W3C: The Get Current URL command returns the URL of the current top-level browsing context.'
+		},
+		{
+			commandName: 'getElementAttribute',
+			commandTitle: 'Get Element Attribute',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/attribute/name',
+			requestBody: '',
+			description: 'W3C: The Get Element Attribute command will return the attribute of a web element.'
+		},
+		{
+			commandName: 'getElementEquals',
+			commandTitle: 'Get Element Equals',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/equals/OTHER_ELEMENT_ID', // TODO: Add support for OTHER_ELEMENT_ID
+			requestBody: '',
+			description: 'JSON Wire Protocol: Test if two element IDs refer to the same DOM element.'
+		},
+		{
+			commandName: 'getElementRect',
+			commandTitle: 'Get Element Rect',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/rect',
+			requestBody: '',
+			description: 'W3C: The Get Element Rect command returns the dimensions and coordinates of the given web element.'
+		},
         {
-            commandName: 'getElementAttribute',
-            commandTitle: 'Get Element Attribute',
-            method: 'GET',
-            path: '/session/SESSION_ID/element/ELEMENT_ID/attribute/name',
-            requestBody: '',
-            description: 'Returns the value of the specified element attribute.'
-        },
-        {
-            commandName: 'getElementScreenshot',
-            commandTitle: 'Get Element Screenshot',
+            commandName: 'takeElementScreenshot',
+            commandTitle: 'Take Element Screenshot',
             method: 'GET',
             path: '/session/SESSION_ID/element/ELEMENT_ID/screenshot',
             requestBody: '',
-            description: 'Returns a screenshot of the specified element.'
+            description: 'W3C: The Take Element Screenshot command takes a screenshot of the visible region encompassed by the bounding rectangle of an element. If given a parameter argument scroll that evaluates to false, the element will not be scrolled into view.'
         },
+		{
+			commandName: 'getElementSize',
+			commandTitle: 'Get Element Size',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/size',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Determine an element\'s size in pixels. The size will be returned as a JSON object with width and height properties.'
+		},
         {
             commandName: 'getElementTagName',
             commandTitle: 'Get Element Tag Name',
             method: 'GET',
             path: '/session/SESSION_ID/element/ELEMENT_ID/name',
             requestBody: '',
-            description: 'Returns the tag name of the specified element.'
+            description: 'W3C: The Get Element Tag Name command returns the qualified tag name name of the given web element.'
         },
         {
             commandName: 'getElementText',
@@ -227,39 +259,183 @@
             method: 'GET',
             path: '/session/SESSION_ID/element/ELEMENT_ID/text',
             requestBody: '',
-            description: 'Returns the inner text of the specified element.'
+            description: 'JSON Wire Protocol: Returns the visible text for the element.'
         },
+		{
+			commandName: 'getElementLocation',
+			commandTitle: 'Get Element Location',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/location',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Determine an element\'s location on the page. The point (0, 0) refers to the upper-left corner of the page. The element\'s coordinates are returned as a JSON object with x and y properties.'
+		},
+		{
+			commandName: 'getElementLocationInView',
+			commandTitle: 'Get Element Location In View',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/location_in_view',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Determine an element\'s location on the screen once it has been scrolled into view.'
+		},
+		{
+			commandName: 'getLocalStorageKeys',
+			commandTitle: 'Get Local Storage Keys',
+			method: 'GET',
+			path: '/session/SESSION_ID/local_storage',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get all keys of the storage.'
+		},
+		{
+			commandName: 'getLocalStorageSize',
+			commandTitle: 'Get Local Storage Size',
+			method: 'GET',
+			path: '/session/SESSION_ID/local_storage/size',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get the number of items in the storage.'
+		},
+		{
+			commandName: 'getLocalStorageKey',
+			commandTitle: 'Get Local Storage Key',
+			method: 'GET',
+			path: '/session/SESSION_ID/local_storage/key/KEY_ID',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get the storage item for the given key.'
+		},
+		{
+			commandName: 'getPageSource',
+			commandTitle: 'Get Page Source',
+			method: 'GET',
+			path: '/session/SESSION_ID/source',
+			requestBody: '',
+			description: 'W3C: The Get Page Source command returns a string serialisation of the DOM of the current browsing context active document.'
+		},
+		{
+			commandName: 'getSessionStorageKeys',
+			commandTitle: 'Get Session Storage Keys',
+			method: 'GET',
+			path: '/session/SESSION_ID/session_storage',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get all keys of the storage.'
+		},
+		{
+			commandName: 'getSessionStorageSize',
+			commandTitle: 'Get Session Storage Size',
+			method: 'GET',
+			path: '/session/SESSION_ID/session_storage/size',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get the number of items in the storage.'
+		},
+		{
+			commandName: 'getSessionStorageKey',
+			commandTitle: 'Get Session Storage Key',
+			method: 'GET',
+			path: 'session/SESSION_ID/session_storage/key/KEY_ID',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get the storage item for the given key.'
+		},
         {
-            commandName: 'getPageTitle',
-            commandTitle: 'Get Page Title',
+            commandName: 'getTitle',
+            commandTitle: 'Get Title',
             method: 'GET',
             path: '/session/SESSION_ID/title',
             requestBody: '',
-            description: 'Returns the title of the page.'
+            description: 'W3C: The Get Title command returns the document title of the current top-level browsing context, equivalent to calling window.top.document.title.'
         },
+		{
+			commandName: 'getWindowHandle',
+			commandTitle: 'Get Window Handle',
+			method: 'GET',
+			path: '/session/SESSION_ID/window_handle',
+			requestBody: '',
+			description: 'W3C: The Get Window Handle command returns the window handle for the current top-level browsing context. It can be used as an argument to Switch To Window.'
+		},
+		{
+			commandName: 'getWindowHandles',
+			commandTitle: 'Get Window Handles',
+			method: 'GET',
+			path: 'session/SESSION_ID/window_handles',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Retrieve the list of all window handles available to the session.'
+		},
+		{
+			commandName: 'getWindowPosition',
+			commandTitle: 'Get Window Position',
+			method: 'GET',
+			path: '/session/SESSION_ID/window/WINDOW_ID/position',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get the position of the specified window. If the :windowHandle URL parameter is "current", the position of the currently active window will be returned.'
+		},
+		{
+			commandName: 'getWindowSize',
+			commandTitle: 'Get Window Size',
+			method: 'GET',
+			path: '/session/SESSION_ID/window/WINDOW_ID/size',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Get the size of the specified window. If the :windowHandle URL parameter is "current", the size of the currently active window will be returned.'
+		},
+		{
+			commandName: 'isElementDisplayed',
+			commandTitle: 'Is Element Displayed',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/displayed',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Determine if an element is currently displayed.'
+		},
+		{
+			commandName: 'isElementEnabled',
+			commandTitle: 'Is Element Enabled',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/enabled',
+			requestBody: '',
+			description: 'W3C: Is Element Enabled determines if the referenced element is enabled or not. This operation only makes sense on form controls.'
+		},
+		{
+			commandName: 'isElementSelected',
+			commandTitle: 'Is Element Selected',
+			method: 'GET',
+			path: '/session/SESSION_ID/element/ELEMENT_ID/selected',
+			requestBody: '',
+			description: 'W3C: Is Element Selected determines if the referenced element is selected or not. This operation only makes sense on input elements of the Checkbox- and Radio Button states, or option elements.'
+		},
+		{
+			commandName: 'maximizeWindow',
+			commandTitle: 'Maximize Window',
+			method: 'POST',
+			path: '/session/SESSION_ID/window/WINDOW_ID/maximize',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Maximize the specified window if not already maximized. If the :windowHandle URL parameter is "current", the currently active window will be maximized.'
+		},
+		{
+			commandName: 'moveTo',
+			commandTitle: 'Move To',
+			method: 'POST',
+			path: '/session/SESSION_ID/moveto',
+			requestBody: '{\"element\": \"ELEMENT_ID\",\"xoffset\": 0,\"yoffset\": 0}',
+			description: 'JSON Wire Protocol: Move the mouse by an offset of the specificed element. If no element is specified, the move is relative to the current mouse cursor. If an element is provided but no offset, the mouse will be moved to the center of the element. If the element is not visible, it will be scrolled into view.'
+		},
         {
             commandName: 'newSession',
-            commandTitle: 'Start Session',
+            commandTitle: 'New Session',
             method: 'POST',
             path: '/session',
             requestBody: '{\"desiredCapabilities\":{},\"requiredCapabilities\":{}}',
-            description: 'Starts a new WebDriver session.'
+            description: 'W3C: The New Session command creates a new WebDriver session with the endpoint node. If the maximum active sessions has been reached, that is if the endpoint node already has a current session, there is a problem processing the given capabilities, or the provisioning of a remote end is impossible, a session not created error is returned.'
         },
         {
-            commandName: 'quit',
-            commandTitle: 'End Session',
+            commandName: 'deleteSession',
+            commandTitle: 'Delete Session',
             method: 'DELETE',
             path: '/session/SESSION_ID',
             requestBody: '',
-            description: 'Ends the WebDriver session.'
+            description: 'W3C: The Delete Session command closes any top-level browsing contexts associated with the current session, terminates the connection, and finally closes the current session.'
         },
         {
-            commandName: 'screenshot',
+            commandName: 'takeScreenshot',
             commandTitle: 'Take Screenshot',
             method: 'GET',
             path: '/session/SESSION_ID/screenshot',
             requestBody: '',
-            description: 'Takes a viewport screenshot.'
+            description: 'W3C: The Take Screenshot command takes a screenshot of the top-level browsing context’s viewport.'
         },
         {
             commandName: 'sendKeys',
@@ -267,8 +443,24 @@
             method: 'POST',
             path: '/session/SESSION_ID/element/ELEMENT_ID/value',
             requestBody: '{\"value\": [\"webdriver\"]}',
-            description: 'Inputs a string into the specified element.'
-        }
+            description: 'JSON Wire Protocol: Send a sequence of key strokes to an element.'
+        },
+		{
+			commandName: 'sendAlertText',
+			commandTitle: 'Send Alert Text',
+			method: 'POST',
+			path: '/session/SESSION_ID/alert_text',
+			requestBody: '{\"text\": \"\"}',
+			description: 'JSON Wire Protocol: Sends keystrokes to a JavaScript prompt() dialog.'
+		},
+		{
+			commandName: 'getSessions',
+			commandTitle: 'Get Sessions',
+			method: 'GET',
+			path: '/sessions',
+			requestBody: '',
+			description: 'JSON Wire Protocol: Returns a list of the currently active sessions.'
+		},
     ];
     
     /* Helper Functions */
